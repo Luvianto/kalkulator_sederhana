2687ba0f0856bba305926c97cad505cb5bbfffcb
Luvianto <lsubrata73@gmail.com>
2023-09-26 22:33:19 +0700

    Menambah Fungsional : Pengulangan input saat eror terjadi

--- bin/kalkulator_sederhana.dart
+++ bin/kalkulator_sederhana.dart
@@ -4,32 +4,18 @@ import 'operasi.dart';
 
 void main() {
   var ulang = 'Y';
+  var operasi = Operasi();
+
+  print('Kalkulator Sederhana');
+  print('====================');
   while (ulang == 'Y') {
-    print('Kalkulator Sederhana');
-    print('====================');
-    try {
-      stdout.write('Masukkan angka pertama :');
-      var bil1 = double.parse(stdin.readLineSync()!);
-      stdout.write('Masukkan angka kedua :');
-      var bil2 = double.parse(stdin.readLineSync()!);
+    masukanAngka1(operasi);
+    masukanAngka2(operasi);
+    masukanPilihan(operasi);
+    aritmatika(operasi);
 
-      print('Pilih jenis operasi aritmatika :');
-      print('1. Tambah');
-      print('2. Kurang');
-      print('3. Kali');
-      print('4. Bagi');
-
-      stdout.write('Pilihan (1/2/3/4) :');
-      var operasi = stdin.readLineSync()!;
-
-      var hitung = Operasi(bil1, bil2, operasi);
-      hitung.aritmatika();
-    } on FormatException {
-      print('Error : Tidak menerima input selain angka');
-    } finally {
-      print('Apakah anda ingin melakukan perhitungan lain?');
-      stdout.write('(Y/T) : ');
-      ulang = stdin.readLineSync()!;
-    }
+    stdout.write('Ingin melakukan operasi lain (Y/N) : ');
+    ulang = stdin.readLineSync()!;
+    print('');
   }
 }
--- bin/operasi.dart
+++ bin/operasi.dart
@@ -1,32 +1,92 @@
+import 'dart:io';
+
 class Operasi {
-  double bil1;
-  double bil2;
-  String operator;
+  double bil1 = 0;
+  double bil2 = 0;
+  int pilihan = 0;
 
-  Operasi(this.bil1, this.bil2, this.operator);
+  set setBil1(double bil1) => this.bil1 = bil1;
+  set setBil2(double bil2) => this.bil2 = bil2;
+  set setPilihan(int pilihan) => this.pilihan = pilihan;
 
-  void aritmatika() {
-    switch (operator) {
-      case '1':
-        print(bil1 + bil2);
-        break;
-      case '2':
-        print(bil1 - bil2);
-        break;
-      case '3':
-        print(bil1 * bil2);
-        break;
-      case '4':
-        try {
-          bil1 ~/ bil2;
-          print(bil1 / bil2);
-        } catch (e) {
-          print('Eror : Angka tidak bisa dibagi nol');
-        }
-        break;
-      default:
-        print('Operasi tidak tersedia');
-        break;
+  void tambah() => print(bil1 + bil2);
+  void kurang() => print(bil1 - bil2);
+  void kali() => print(bil1 * bil2);
+  void bagi() {
+    try {
+      bil1 ~/ bil2;
+      print(bil1 / bil2);
+    } catch (e) {
+      print('Eror : Angka tidak bisa dibagi nol');
     }
+  }
+}
+
+// note untuk ke depan :
+// Semua fungsi dibawah mungkin bisa dijadikan method class Operasi,
+// tapi sedikit malas karena sudah terlanjur
+
+void aritmatika(Operasi operasi) {
+  stdout.write('Hasil : ');
+  switch (operasi.pilihan) {
+    case 1:
+      operasi.tambah();
+      break;
+    case 2:
+      operasi.kurang();
+      break;
+    case 3:
+      operasi.kali();
+      break;
+    case 4:
+      operasi.bagi();
+      break;
+    default:
+      print('Operasi tidak tersedia');
+      masukanPilihan(operasi);
+      break;
+  }
+}
+
+void masukanAngka1(Operasi operasi) {
+  try {
+    stdout.write('Masukkan angka pertama : ');
+    var bil1 = double.parse(stdin.readLineSync()!);
+    operasi.setBil1 = bil1;
+    print('');
+  } on FormatException {
+    print('Error : Tidak menerima input selain angka');
+    masukanAngka1(operasi);
+  }
+}
+
+void masukanAngka2(Operasi operasi) {
+  try {
+    stdout.write('Masukkan angka kedua   : ');
+    var bil2 = double.parse(stdin.readLineSync()!);
+    operasi.setBil2 = bil2;
+    print('');
+  } on FormatException {
+    print('Error : Tidak menerima input selain angka');
+    masukanAngka2(operasi);
+  }
+}
+
+void masukanPilihan(Operasi operasi) {
+  try {
+    print('Pilih jenis operasi aritmatika :');
+    print('1. Tambah');
+    print('2. Kurang');
+    print('3. Kali');
+    print('4. Bagi');
+    print('');
+
+    stdout.write('Pilihan (1/2/3/4) :');
+    var pilihan = int.parse(stdin.readLineSync()!);
+    operasi.setPilihan = pilihan;
+    print('');
+  } on FormatException {
+    print('Eror : Masukan angka sesuai pilihan yang tersedia');
+    masukanPilihan(operasi);
   }
 }
